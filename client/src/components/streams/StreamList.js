6fc598dbe923184e461bb8b053fa65e290da7168
rhladchenko <ruslan.hladchenko@cpcs.ws>
2021-01-06 20:52:55 +0200

    Next streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,8 +12,18 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className='right floated content'>
-                    <Link to={`/streams/edit/${stream.id}`} className='ui button primary'>Edit</Link>
-                    <Link to={`/streams/delete/${stream.id}`} className='ui button negative'>Delete</Link>
+                    <Link
+                        to={`/streams/edit/${stream.id}`}
+                        className='ui button primary'
+                    >
+                        Edit
+                    </Link>
+                    <Link
+                        to={`/streams/delete/${stream.id}`}
+                        className='ui button negative'
+                    >
+                        Delete
+                    </Link>
                 </div>
             );
         }
@@ -21,13 +31,19 @@ class StreamList extends React.Component {
 
     renderList() {
         return this.props.streams.map((stream) => {
+            if (this.props.currentUserId !== stream.userId) {
+                return null;
+            }
+
             return (
                 <div className='item' key={stream.id}>
                     {this.renderAdmin(stream)}
 
                     <i className='large middle aligned icon camera' />
                     <div className='content'>
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className='header'>
+                            {stream.title}
+                        </Link>
                         <div className='description'>{stream.description}</div>
                     </div>
                 </div>
